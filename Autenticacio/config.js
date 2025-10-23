d1b28dbf0b0eeed85907301b9dec3bff672f5311
MartiVilas <marti.vilas14@gmail.com>
2025-10-23 15:52:40 +0200

    create vue project

--- Autenticacio/config.js
+++
@@ -1,5 +0,0 @@
-export const {
-    PORT = 3000,
-    SALT_ROUNDS=5,
-    SECRET_JWT_KEY="123456789"
-} = process.env;
--- Login/config.js
+++
@@ -1,5 +0,0 @@
-export const {
-    PORT=3000,
-    SALT_ROUNDS=10 ,
-    SECRET_JWT_KEY="aixo-es-una-super-paraula-secreta" 
-}=process.env
